d947514e382e170f127b619ef484dc17b58538c7
yuanzhou <yuanzhou0110@qq.com>
2026-09-06 01:05:16 +0800

    add log

--- app/service/deal_lon.py
+++ app/service/deal_lon.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 
@@ -7,12 +8,14 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
+from app.core.http import logger
 from app.service.area import get_all_area_names, get_area_by_name, update_area_by_id
 
 base_url = "https://restapi.amap.com/v3/geocode/geo"
 
 
 def get_position(db:Session = Depends(get_db)):
+    logger.info("开始同步经纬度")
     areas = get_all_area_names()
     key = os.getenv("AMAP_KAY")
     params_list = []
@@ -24,7 +27,9 @@ def get_position(db:Session = Depends(get_db)):
             }
         )
     for param in params_list:
+        logger.info("请求信息: url: " + base_url + "params： " + param)
         resp = requests.get(url=base_url, params=param)
+        logger.info("返回信息: " + resp.text)
         if resp.status_code == 200:
             area = get_area_by_name(param['name'])
             data = resp.json()
